f9c1c93da6ab15ea3c462ff8f23190ad102dc659
Gabriel Pinochet-Soto <gpin2@pdx.edu>
2025-01-21 02:13:54 -0800

    Add basic model JAX working

--- src/models.py
+++ src/models.py
@@ -1,15 +1,20 @@
 """
-Impelemntation of basic linear models for encoder-decoder architectures
-in the context of multilevel methods.
-Using JAX for the implementation.
+Implementation of basic linear models for encoder-decoder architectures
+using JAX.
 """
 
+from functools import partial
 import jax
 import jax.numpy as jnp
-from jax import random, vmap, grad, jit
-import typing as tp
+from jax import random, jit
 
+# Parameters
 __NUM_EPOCHS__ = 100
+__COARSE_DIM__ = 5
+__FINE_DIM__ = 10
+__BATCH_SIZE__ = 5
+__NUM_BATCHES__ = 50
+__LEARNING_RATE__ = 0.05
 
 
 class DenseEncoderDecoder:
@@ -17,156 +22,58 @@ class DenseEncoderDecoder:
     Encoder-Decoder architecture using dense layers.
     """
 
-    def __init__(
-        self,
-        fine_dim: int,
-        coarse_dim: int,
-    ):
-        """
-        Initialize the encoder-decoder architecture.
-
-        Args:
-            fine_dim: int
-                Dimension of the fine grid.
-            coarse_dim: int
-                Dimension of the coarse grid.
-        """
+    def __init__(self, fine_dim: int, coarse_dim: int):
         self.fine_dim = fine_dim
         self.coarse_dim = coarse_dim
+        self.key = random.PRNGKey(0)
+        self.encoder_weights = random.normal(self.key, (fine_dim, coarse_dim))
+        self.decoder_weights = random.normal(self.key, (coarse_dim, fine_dim))
 
-        self.__init_weights()
+    def encode(self, x: jnp.ndarray) -> jnp.ndarray:
+        return jnp.dot(x, self.encoder_weights)
 
-    # TODO: Use enum for different initialization methods
-    def __init_weights(self):
-        """
-        Initialize the weights of the network.
-        """
-        key = random.PRNGKey(0)
-        self.encoder_weights = random.normal(
-            key, (self.fine_dim, self.coarse_dim)
-        )
-        self.decoder_weights = random.normal(
-            key, (self.coarse_dim, self.fine_dim)
-        )
+    def decode(self, x: jnp.ndarray) -> jnp.ndarray:
+        return jnp.dot(x, self.decoder_weights)
 
-    def encoder(
-        self,
-        x: jnp.ndarray,
-        encoder_weights: jnp.ndarray,
-    ) -> jnp.ndarray:
-        """
-        Encoder function.
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        return self.decode(self.encode(x))
 
-        Args:
-            x: jnp.ndarray
-                Input data.
 
-        Returns:
-            jnp.ndarray
-                Encoded data.
-        """
-        return jnp.dot(x, encoder_weights)
+def loss(x, y, encoder_weights, decoder_weights, coarse_dim=5, alpha=1.0e-3):
+    """
+    Loss function for encoder-decoder architecture.
+    """
+    reconstruction_loss = jnp.mean((x - y) ** 2)
+    regularization_loss = jnp.linalg.norm(encoder_weights)
+    regularization_loss += jnp.linalg.norm(decoder_weights)
+    identity_loss = jnp.linalg.norm(
+        jnp.eye(coarse_dim) - jnp.dot(decoder_weights, encoder_weights)
+    )
+    return reconstruction_loss + alpha * (regularization_loss + identity_loss)
 
-    def decoder(
-        self,
-        x: jnp.ndarray,
-        decoder_weights: jnp.ndarray,
-    ) -> jnp.ndarray:
-        """
-        Decoder function.
 
-        Args:
-            x: jnp.ndarray
-                Input data.
-
-        Returns:
-            jnp.ndarray
-                Decoded data.
-        """
-        return jnp.dot(x, decoder_weights)
-
-    def __call__(
-        self,
-        x: jnp.ndarray,
-        encoder_weights: jnp.ndarray,
-        decoder_weights: jnp.ndarray,
-    ) -> jnp.ndarray:
-        """
-        Forward pass of the network.
-
-        Args:
-            x: jnp.ndarray
-                Input data.
-
-        Returns:
-            jnp.ndarray
-                Output data.
-        """
-        return self.decoder(self.encoder(x, encoder_weights), decoder_weights)
-
-    def loss(
-        self,
-        x: jnp.ndarray,
-        encoder_weights: jnp.ndarray,
-        decoder_weights: jnp.ndarray,
-    ) -> jnp.ndarray:
-        """
-        Loss function.
-        MSE + norm of the weights
-        + norm of the product of the weights minus the identity matrix.
-
-        Args:
-            x: jnp.ndarray
-                Input data.
-
-        Returns:
-            jnp.ndarray
-                Loss value.
-        """
-        return (
-            jnp.mean((x - self(x, encoder_weights, decoder_weights)) ** 2)
-            + jnp.linalg.norm(encoder_weights)
-            + jnp.linalg.norm(decoder_weights)
-            + jnp.linalg.norm(
-                jnp.eye(self.coarse_dim)
-                - jnp.dot(decoder_weights, encoder_weights)
-            )
-        )
-
-    def update(self, x: jnp.ndarray, lr: float = 0.01) -> None:
-        """
-        Update the weights of the network.
-
-        Args:
-            x: jnp.ndarray
-                Input data.
-            lr: float
-                Learning rate.
-        """
-        loss, (grad_encoder, grad_decoder) = jax.value_and_grad(
-            self.loss, (1, 2)
-        )(x, self.encoder_weights, self.decoder_weights)
-        self.encoder_weights -= lr * grad_encoder
-        self.decoder_weights -= lr * grad_decoder
+def update(x, encoder_decoder, lr=0.01):
+    loss_value, grads = jax.value_and_grad(loss, argnums=(2, 3))(
+        x,
+        encoder_decoder(x),
+        encoder_decoder.encoder_weights,
+        encoder_decoder.decoder_weights,
+    )
+    encoder_decoder.encoder_weights -= lr * grads[0]
+    encoder_decoder.decoder_weights -= lr * grads[1]
+    return loss_value
 
 
 if __name__ == "__main__":
-    # Test the implementation
-    encoder_decoder = DenseEncoderDecoder(10, 5)
-    x = jnp.ones((10, 10))
-    print(
-        f"Initial loss: {encoder_decoder.loss(x, encoder_decoder.encoder_weights, encoder_decoder.decoder_weights)}\n",
-        f"Initial output: {encoder_decoder(x, encoder_decoder.encoder_weights, encoder_decoder.decoder_weights)}\n",
-    )
+    encoder_decoder = DenseEncoderDecoder(__FINE_DIM__, __COARSE_DIM__)
+    key = random.PRNGKey(0)
+    x = random.normal(key, (__BATCH_SIZE__, __FINE_DIM__))
     for epoch in range(__NUM_EPOCHS__):
-        encoder_decoder.update(x)
+        for _ in range(__NUM_BATCHES__):
+            loss_value = update(x, encoder_decoder, __LEARNING_RATE__)
         if epoch % 10 == 0:
-            print(
-                f"\tEpoch: {epoch}/{__NUM_EPOCHS__}\n",
-                f"\tLoss: {encoder_decoder.loss(x, encoder_decoder.encoder_weights, encoder_decoder.decoder_weights)}\n",
-                f"\tOutput: {encoder_decoder(x, encoder_decoder.encoder_weights, encoder_decoder.decoder_weights)}\n",
-            )
-    print(
-        f"Final loss: {encoder_decoder.loss(x, encoder_decoder.encoder_weights, encoder_decoder.decoder_weights)}\n",
-        f"Final output: {encoder_decoder(x, encoder_decoder.encoder_weights, encoder_decoder.decoder_weights)}\n",
-    )
+            print(f"\nEpoch: {epoch}")
+            print(f"\n\tLoss: {loss_value}")
+            print(f"\n\tEncoder weights: {encoder_decoder.encoder_weights}")
+            print(f"\n\tDecoder weights: {encoder_decoder.decoder_weights}")
+    print("\nTraining complete!\n")
