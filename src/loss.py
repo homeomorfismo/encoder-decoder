5bc74399045549f7463d9c895b06b2db12724c0a
Gabriel Pinochet-Soto <gpin2@pdx.edu>
2025-01-26 11:56:09 -0800

    Gather wrappers into utils, refactor losses

--- src/loss.py
+++ src/loss.py
@@ -3,197 +3,109 @@ Implementation of loss functions for encoder-decoder architectures
 using JAX.
 """
 
-# Note: Enum and get_loss_function at the end of the file
 import jax
 import jax.numpy as jnp
 from jax import jit
 from enum import Enum
-from typing import Callable
+from typing import Callable, Union
 
 
-######################
-# L2 Loss functions
-######################
-@jit
-def loss_euclidean(
-    x: jnp.ndarray,
-    y: jnp.ndarray,
-    encoder_weights: jnp.ndarray,
-    decoder_weights: jnp.ndarray,
-    reg: float,
-) -> float:
-    """
-    Loss function for encoder-decoder architecture.
-    * MSE loss for reconstruction.
-    * L2 regularization on encoder and decoder weights.
-    """
-    reconstr_loss = jnp.mean((x - y) ** 2)
-    reg_loss = jnp.linalg.norm(encoder_weights, ord=2)
-    reg_loss += jnp.linalg.norm(decoder_weights, ord=2)
-    reg_loss += jnp.linalg.norm(
-        jnp.eye(decoder_weights.shape[0])
-        - jnp.dot(decoder_weights, encoder_weights),
-        ord=2,
-    )
-    return reconstr_loss + reg * reg_loss
-
-
-@jit
-def loss_mg_euclidean(
-    x: jnp.ndarray,
-    y: jnp.ndarray,
-    encoder_weights: jnp.ndarray,
-    decoder_weights: jnp.ndarray,
-    reg: float,
-) -> float:
-    """
-    Loss function for MG encoder-decoder architecture.
-    * MSE loss for reconstruction.
-    * L2 regularization on encoder and decoder weights.
-    """
-    reconstr_loss = jnp.mean(
-        jnp.dot(x - y, jnp.transpose(decoder_weights)) ** 2
-    )
-    reg_loss = jnp.linalg.norm(encoder_weights, ord=2)
-    reg_loss += jnp.linalg.norm(decoder_weights, ord=2)
-    reg_loss += jnp.linalg.norm(
-        jnp.eye(decoder_weights.shape[0])
-        - jnp.dot(decoder_weights, encoder_weights),
-        ord=2,
-    )
-    return reconstr_loss + reg * reg_loss
-
-
-######################
-# L1 Loss functions
-######################
-@jit
-def loss_l1(
-    x: jnp.ndarray,
-    y: jnp.ndarray,
-    encoder_weights: jnp.ndarray,
-    decoder_weights: jnp.ndarray,
-    reg: float,
-) -> float:
-    """
-    Loss function for encoder-decoder architecture.
-    * MSE loss for reconstruction.
-    * L1 regularization on encoder and decoder weights.
-    """
-    reconstr_loss = jnp.mean((x - y) ** 2)
-    reg_loss = jnp.linalg.norm(encoder_weights, ord=1)
-    reg_loss += jnp.linalg.norm(decoder_weights, ord=1)
-    reg_loss += jnp.linalg.norm(
-        jnp.eye(decoder_weights.shape[0])
-        - jnp.dot(decoder_weights, encoder_weights),
-        ord=1,
-    )
-    return reconstr_loss + reg * reg_loss
-
-
-@jit
-def loss_mg_l1(
-    x: jnp.ndarray,
-    y: jnp.ndarray,
-    encoder_weights: jnp.ndarray,
-    decoder_weights: jnp.ndarray,
-    reg: float,
-) -> float:
-    """
-    Loss function for MG encoder-decoder architecture.
-    * MSE loss for reconstruction.
-    * L1 regularization on encoder and decoder weights.
-    """
-    reconstr_loss = jnp.mean(
-        jnp.dot(x - y, jnp.transpose(decoder_weights)) ** 2
-    )
-    reg_loss = jnp.linalg.norm(encoder_weights, ord=1)
-    reg_loss += jnp.linalg.norm(decoder_weights, ord=1)
-    reg_loss += jnp.linalg.norm(
-        jnp.eye(decoder_weights.shape[0])
-        - jnp.dot(decoder_weights, encoder_weights),
-        ord=1,
-    )
-    return reconstr_loss + reg * reg_loss
-
-
-######################
-# L0 Loss functions
-######################
-@jit
-def loss_l0(
-    x: jnp.ndarray,
-    y: jnp.ndarray,
-    encoder_weights: jnp.ndarray,
-    decoder_weights: jnp.ndarray,
-    reg: float,
-) -> float:
-    """
-    Loss function for encoder-decoder architecture.
-    * MSE loss for reconstruction.
-    * L0 regularization on encoder and decoder weights.
-    """
-    reconstr_loss = jnp.mean((x - y) ** 2)
-    reg_loss = jnp.sum(jnp.abs(encoder_weights) > 0)
-    reg_loss += jnp.sum(jnp.abs(decoder_weights) > 0)
-    reg_loss += jnp.sum(
-        jnp.abs(
-            jnp.eye(decoder_weights.shape[0])
-            - jnp.dot(decoder_weights, encoder_weights)
-        )
-        > 0
-    )
-    return reconstr_loss + reg * reg_loss
-
-
-@jit
-def loss_mg_l0(
-    x: jnp.ndarray,
-    y: jnp.ndarray,
-    encoder_weights: jnp.ndarray,
-    decoder_weights: jnp.ndarray,
-    reg: float,
-) -> float:
-    """
-    Loss function for MG encoder-decoder architecture.
-    * MSE loss for reconstruction.
-    * L0 regularization on encoder and decoder weights.
-    """
-    reconstr_loss = jnp.mean(
-        jnp.dot(x - y, jnp.transpose(decoder_weights)) ** 2
-    )
-    reg_loss = jnp.sum(jnp.abs(encoder_weights) > 0)
-    reg_loss += jnp.sum(jnp.abs(decoder_weights) > 0)
-    reg_loss += jnp.sum(
-        jnp.abs(
-            jnp.eye(decoder_weights.shape[0])
-            - jnp.dot(decoder_weights, encoder_weights)
-        )
-        > 0
-    )
-    return reconstr_loss + reg * reg_loss
-
-
-class LossFunctionType(Enum):
-    EUCLIDEAN = loss_euclidean
-    MG_EUCLIDEAN = loss_mg_euclidean
-    L1 = loss_l1
-    MG_L1 = loss_mg_l1
-    L0 = loss_l0
-    MG_L0 = loss_mg_l0
-
-
-def get_loss_function(loss_function_type: LossFunctionType) -> Callable:
+def get_loss(ord: Union[int, float, str, None]) -> Callable:
     """
     Get the loss function based on the specified type.
 
     Args:
-    - loss_function_type (LossFunctionType): The type of loss function to use.
+    - ord (Union[int, float, jnp.inf, -jnp.inf, 'fro', 'nuc', None]): The type of loss function to use.
 
     Returns:
     - Callable: The JAX loss function.
     """
-    return loss_function_type.value
+
+    @jit
+    def loss(
+        x: jnp.ndarray,
+        y: jnp.ndarray,
+        encoder_weights: jnp.ndarray,
+        decoder_weights: jnp.ndarray,
+        reg: float,
+    ) -> float:
+        """
+        Loss function for encoder-decoder architecture.
+        * MSE loss for reconstruction.
+        * Regularization on encoder and decoder weights.
+        """
+        reconstr_loss = jnp.mean((x - y) ** 2)
+        if ord == 0:
+            reg_loss = jnp.sum(jnp.abs(encoder_weights) > 0)
+            reg_loss += jnp.sum(jnp.abs(decoder_weights) > 0)
+            reg_loss += jnp.sum(
+                jnp.abs(
+                    jnp.eye(decoder_weights.shape[0])
+                    - jnp.dot(decoder_weights, encoder_weights)
+                )
+                > 0
+            )
+        else:
+            reg_loss = jnp.linalg.norm(encoder_weights, ord=ord)
+            reg_loss += jnp.linalg.norm(decoder_weights, ord=ord)
+            reg_loss += jnp.linalg.norm(
+                jnp.eye(decoder_weights.shape[0])
+                - jnp.dot(decoder_weights, encoder_weights),
+                ord=ord,
+            )
+        return reconstr_loss + reg * reg_loss
+
+    return loss
+
+
+def get_mg_loss(ord: Union[int, float, str, None]) -> Callable:
+    """
+    Get the loss function based on the specified type.
+
+    Args:
+    - ord (Union[int, float, inf, -inf, 'fro', 'nuc', None]): The type of loss function to use.
+
+    Returns:
+    - Callable: The JAX loss function.
+    """
+
+    @jit
+    def loss_mg(
+        x: jnp.ndarray,
+        y: jnp.ndarray,
+        encoder_weights: jnp.ndarray,
+        decoder_weights: jnp.ndarray,
+        reg: float,
+    ) -> float:
+        """
+        Loss function for MG encoder-decoder architecture.
+        * MSE loss for reconstruction.
+        * Regularization on encoder and decoder weights.
+        """
+        reconstr_loss = jnp.mean(
+            jnp.dot(x - y, jnp.transpose(decoder_weights)) ** 2
+        )
+        if ord == 0:
+            reg_loss = jnp.sum(jnp.abs(encoder_weights) > 0)
+            reg_loss += jnp.sum(jnp.abs(decoder_weights) > 0)
+            reg_loss += jnp.sum(
+                jnp.abs(
+                    jnp.eye(decoder_weights.shape[0])
+                    - jnp.dot(decoder_weights, encoder_weights)
+                )
+                > 0
+            )
+        else:
+            reg_loss = jnp.linalg.norm(encoder_weights, ord=ord)
+            reg_loss += jnp.linalg.norm(decoder_weights, ord=ord)
+            reg_loss += jnp.linalg.norm(
+                jnp.eye(decoder_weights.shape[0])
+                - jnp.dot(decoder_weights, encoder_weights),
+                ord=ord,
+            )
+        return reconstr_loss + reg * reg_loss
+
+    return loss_mg
 
 
 def __test_loss_functions():
@@ -205,71 +117,17 @@ def __test_loss_functions():
     decoder_weights = np.random.rand(10, 10)
     reg = 0.1
 
-    names = [
-        "EUCLIDEAN",
-        "MG_EUCLIDEAN",
-        "L1",
-        "MG_L1",
-        "L0",
-        "MG_L0",
-    ]
-    values = []
-    values.append(loss_euclidean(x, y, encoder_weights, decoder_weights, reg))
-    values.append(
-        loss_mg_euclidean(x, y, encoder_weights, decoder_weights, reg)
-    )
-    values.append(loss_l1(x, y, encoder_weights, decoder_weights, reg))
-    values.append(loss_mg_l1(x, y, encoder_weights, decoder_weights, reg))
-    values.append(loss_l0(x, y, encoder_weights, decoder_weights, reg))
-    values.append(loss_mg_l0(x, y, encoder_weights, decoder_weights, reg))
+    ord_list = [0, 1, 2, jnp.inf, -jnp.inf, "fro", "nuc", None]
 
-    for name, value in zip(names, values):
-        print(f"{name}:\t{value}\n")
-
-
-def __test_grads_loss_functions():
-    import numpy as np
-
-    x = np.random.rand(10, 10)
-    y = np.random.rand(10, 10)
-    encoder_weights = np.random.rand(10, 10)
-    decoder_weights = np.random.rand(10, 10)
-    reg = 0.1
-
-    names = [
-        "EUCLIDEAN",
-        "MG_EUCLIDEAN",
-        "L1",
-        "MG_L1",
-        "L0",
-        "MG_L0",
-    ]
-    values = []
-    values.append(
-        jax.grad(loss_euclidean)(x, y, encoder_weights, decoder_weights, reg)
-    )
-    values.append(
-        jax.grad(loss_mg_euclidean)(
-            x, y, encoder_weights, decoder_weights, reg
+    for ord in ord_list:
+        loss = get_loss(ord)
+        loss_mg = get_mg_loss(ord)
+        print(f"Testing loss function with ord={ord}")
+        print(f"Loss: {loss(x, y, encoder_weights, decoder_weights, reg)}")
+        print(
+            f"MG Loss: {loss_mg(x, y, encoder_weights, decoder_weights, reg)}"
         )
-    )
-    values.append(
-        jax.grad(loss_l1)(x, y, encoder_weights, decoder_weights, reg)
-    )
-    values.append(
-        jax.grad(loss_mg_l1)(x, y, encoder_weights, decoder_weights, reg)
-    )
-    values.append(
-        jax.grad(loss_l0)(x, y, encoder_weights, decoder_weights, reg)
-    )
-    values.append(
-        jax.grad(loss_mg_l0)(x, y, encoder_weights, decoder_weights, reg)
-    )
-
-    for name, value in zip(names, values):
-        print(f"{name}:\t{value}\n")
 
 
 if __name__ == "__main__":
     __test_loss_functions()
-    __test_grads_loss_functions()
--- src/utilities.py
+++ src/utilities.py
@@ -1,11 +1,13 @@
 """
-Wrappers for Optax optimizers.
-(This is moreso for a list/reminder of available optimizers in Optax.)
+Wrappers for Optax optimizers and JAX initializers.
+
 See https://optax.readthedocs.io/en/latest/api/optimizers.html
+and https://jax.readthedocs.io/en/latest/jax.nn.html#initializers
 """
 
 from enum import Enum
 import optax
+import jax.nn.initializers as jinit
 from typing import Callable
 
 
@@ -29,13 +31,31 @@ class OptimizerType(Enum):
     NOISY_SGD = optax.noisy_sgd
     NOVOGRAD = optax.novograd
     OPTIMISTIC_GRADIENT_DESCENT = optax.optimistic_gradient_descent
-    POLYAK_SDG = optax.polyak_sgd
+    POLYAK_SGD = optax.polyak_sgd
     RADAM = optax.radam
     RMSPROP = optax.rmsprop
     SGD = optax.sgd
     SIGN_SGD = optax.sign_sgd
     SM3 = optax.sm3
     YOGI = optax.yogi
+
+
+class InitializerType(Enum):
+    CONSTANT = jinit.constant
+    DELTA_ORTHOGONAL = jinit.delta_orthogonal
+    GLOROT_NORMAL = jinit.glorot_normal
+    GLOROT_UNIFORM = jinit.glorot_uniform
+    HE_NORMAL = jinit.he_normal
+    HE_UNIFORM = jinit.he_uniform
+    LECUN_NORMAL = jinit.lecun_normal
+    LECUN_UNIFORM = jinit.lecun_uniform
+    NORMAL = jinit.normal
+    ONES = jinit.ones
+    ORTHOGONAL = jinit.orthogonal
+    TRUNCATED_NORMAL = jinit.truncated_normal
+    UNIFORM = jinit.uniform
+    VARIANCE_SCALING = jinit.variance_scaling
+    ZEROS = jinit.zeros
 
 
 def get_optimizer(optimizer_type: OptimizerType, *args, **kwargs) -> Callable:
@@ -53,7 +73,27 @@ def get_optimizer(optimizer_type: OptimizerType, *args, **kwargs) -> Callable:
     return optimizer_type(*args, **kwargs)
 
 
+def get_initializer(
+    initializer_type: InitializerType, *args, **kwargs
+) -> Callable:
+    """
+    Get the initializer function from JAX based on the specified type.
+
+    Args:
+    - initializer_type (InitializerType): The type of initializer to use.
+    - *args: Positional arguments to pass to the initializer function.
+    - **kwargs: Keyword arguments to pass to the initializer function.
+
+    Returns:
+    - Callable: The JAX initializer function.
+    """
+    return initializer_type(*args, **kwargs)
+
+
 # Example usage
 if __name__ == "__main__":
     optimizer = get_optimizer(OptimizerType.ADAM, learning_rate=0.001)
     print(optimizer)
+
+    initializer = get_initializer(InitializerType.GLOROT_UNIFORM)
+    print(initializer)
